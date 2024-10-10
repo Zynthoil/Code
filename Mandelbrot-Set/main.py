b8488cce4067d7defa614f043679788bd04a23ce
Zynthoil <samtmoores@icloud.com>
2024-10-10 21:50:30 +0100

    -

--- Mandelbrot-Set/main.py
+++ Mandelbrot-Set/main.py
@@ -1,13 +1,18 @@
-# Size of Mandelbrot set
-width = 100
-height = 100
+# Resolution of mandelbrot set, must be set to multiple of 5
+resolution = 20
+resolutionX = resolution
+resolutionY = int(resolution)
 
 # Create complex plane
-complexPlane = [[' ' for x in range(width)] for y in range(height)]
+complexPlane = [[' ' for y in range(resolutionY)] for x in range(resolutionX)]
 
 # Given two floats from the complex plane, check if the corresponding complex number is in the mandelbrot set
 # Not perfect, but good enough!
 def CheckComplexNumber(a, b):
+    print(a, b, end=' | ')
+    a = (a / ((resolutionX - 1) / 2)) - 1.5
+    b = (b / ((resolutionY - 1) / 2)) - 1
+    print(a, b)
     z = 0
     c = complex(a, b)
     initialAbs = abs(c)
@@ -23,17 +28,18 @@ def CheckComplexNumber(a, b):
 
 # Prints the plane
 def PrintPlane(plane):
-    for x in range(len(plane)):
-        for y in range(len(plane)):
-            print(plane[x][y], end = '')
+    for y in range(len(plane) - 1, -1, -1):
+        for x in range(len(plane)):
+            print(plane[x][y], end='')
         print()
 
 # Assigns each ascii value depending on the CheckComplexNumber function
 def CreateMandelbrotSet():
-    for x in range(len(complexPlane)):
-        for y in range(len(complexPlane)):
+    for y in range(len(complexPlane)):
+        for x in range(len(complexPlane)):
             if CheckComplexNumber(x, y):
                 complexPlane[x][y] = '#'
+            # complexPlane[x][y] = '|x' + str(x) + 'y' + str(y) + '|'
 
 CreateMandelbrotSet()
 PrintPlane(complexPlane)
