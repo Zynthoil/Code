83b228171b7ea257fb1b8b8ebc163aa966fd174e
Zynthoil <samtmoores@icloud.com>
2024-10-14 23:22:53 +0100

    Added high res BW image

--- Mandelbrot-Set/pixels.py
+++ Mandelbrot-Set/pixels.py
@@ -4,32 +4,26 @@ resolution = 10001
 
 img = Image.new('RGB', (resolution, resolution), color='white')
 
-
 def CheckComplexNumber(a, b):
-    # print(a, b, end=' | ')
     a = a / ((resolution-1)/4) - 2
     b = b / ((resolution-1)/4) - 2
-    # print(a, b)
     z = 0
     c = complex(a, b)
     initialAbs = abs(c)
-    # Run loop to check recursive function
     for i in range(100):
         z = z * z + c
         if abs(z) > 2: return False
-    finalAbs = abs(z)
-    # Check if absolute value of complex number tends towards infinity
-    if finalAbs > 2:
-        return False
-    else:
-        return True
+    return True
 
 def CreateMandelbrotSet():
+    loops = 0
     for y in range(resolution):
         for x in range(resolution):
             if CheckComplexNumber(x, y):
                 img.putpixel( (x, y), (0, 0, 0))
+            loops += 1
+            print(str(loops / resolution ** 2 * 100) + '%')
 
 CreateMandelbrotSet()
-# img.save("Mandelbrot-Set-BW", "png")
+img.save("Mandelbrot-Set-BW", "png")
 img.show()
