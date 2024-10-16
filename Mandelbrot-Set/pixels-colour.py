839fffe247b672a179d77a7ee5a02b5f210566c1
Zynthoil <samtmoores@icloud.com>
2024-10-16 19:31:36 +0100

    Made mandelbrot set twice as efficient

--- Mandelbrot-Set/pixels-colour.py
+++ Mandelbrot-Set/pixels-colour.py
@@ -16,25 +16,27 @@ def CheckComplexNumber(a, b):
     a = a / ((resolution-1)/4) - 2
     b = b / ((resolution-1)/4) - 2
     z = 0
-    n = 0
     c = complex(a, b)
     initialAbs = abs(c)
     for i in range(100):
         z = z ** d + c
-        n += 1
-        if abs(z) > 2: return GenerateColour(n)
+        if abs(z) > 2: return GenerateColour(i)
     return (0, 0, 0)
 
 def CreateMandelbrotSet():
     loops = 0
-    for y in range(resolution):
+    if resolution % 2 != 0:
+        resolutionY = (resolution - 1) / 2 + 1
+    else:
+        resolutionY = resolution / 2 + 1
+    for y in range(int(resolutionY)):
         for x in range(resolution):
             colour = CheckComplexNumber(x, y)
             img.putpixel( (x, y), colour)
+            img.putpixel( (x, y * -1), colour)
         loops += 1
-        percentage = loops / resolution * 100
+        percentage = loops / resolutionY * 100
         print(str(round(percentage, 2)) + '%')
-        # print(str(round(loops / resolution * 100), 2) + '%')
 
 startTime = time.perf_counter()
 CreateMandelbrotSet()
